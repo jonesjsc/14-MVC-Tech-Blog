5d462d4116000cdfc36eebd4df3c3747512bfaa0
Jason Jones <jason.e.jones@gmail.com>
2021-05-02 12:00:10 -0400

    post route enabled and root get works

--- controllers/api/postRoutes.js
+++ controllers/api/postRoutes.js
@@ -1,6 +1,32 @@
 const router = require('express').Router();
-const { Project } = require('../../models');
+const { Post, User, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
+
+router.get('/', async (req, res) => {
+  try {
+    const postData = await Post.findAll({
+      attributes: ['id', 'title', 'content', 'created_at'],
+      order: [['created_at', 'DESC']],
+      include: [
+        { model: User, attributes: ['username'] },
+        {
+          model: Comment,
+          attributes: [
+            'id',
+            'comment_text',
+            'post_id',
+            'user_id',
+            'created_at',
+          ],
+          include: { model: User, attributes: ['username'] },
+        },
+      ],
+    });
+    res.status(200).json(postData.reverse());
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 
 router.post('/', withAuth, async (req, res) => {
   try {
